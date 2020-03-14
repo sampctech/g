100e02262423df3e780f88e77f4c80f5003362ec
Sam Barna <54453917+sampctech@users.noreply.github.com>
2020-03-13 21:54:15 -0400

    Update getgit.sh

--- getgit.sh
+++ getgit.sh
@@ -7,7 +7,7 @@
 #####################################################################
 
 #git clone https://github.com/sampctech/g "$HOME"/g
-git clone https://github.com/sampctech/1 "$HOME"/github/1
+git clone https://github.com/sampctech/arccin "$HOME"/github/arccin
 git clone https://github.com/sampctech/mcy "$HOME"/github/mcy
 git clone https://github.com/sampctech/org "$HOME"/github/org
 
