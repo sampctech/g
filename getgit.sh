722b726379c77d633c10439808a097ff86b96636
Sam Barna <54453917+sampctech@users.noreply.github.com>
2020-03-13 21:39:51 -0400

    Update getgit.sh

--- getgit.sh
+++ getgit.sh
@@ -6,13 +6,12 @@
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #####################################################################
 
+#git clone https://github.com/sampctech/g "$HOME"/g
 git clone https://github.com/sampctech/1 "$HOME"/github/1
-#git clone https://github.com/sampctech/2  "$HOME"/github/2
-#git clone https://github.com/sampctech/game "$HOME"/github/game
-#git clone https://github.com/sampctech/mlt "$HOME"/github/mlt
+git clone https://github.com/sampctech/mcy "$HOME"/github/mcy
 git clone https://github.com/sampctech/org "$HOME"/github/org
 
-git clone https://github.com/arcolinuxd/arco-cinnamon "$HOME"/
+git clone https://github.com/arcolinuxd/arco-cinnamon "$HOME"/arco-cinnamon
 
 chmod -R 755 "$HOME"/github
 exit
