e226a563b5c981aa1828a8d2463e2ba8f3f3a05e
Sam Barna <54453917+sampctech@users.noreply.github.com>
2020-03-13 21:25:00 -0400

    Update getgit.sh

--- getgit.sh
+++ getgit.sh
@@ -7,9 +7,9 @@
 #####################################################################
 
 git clone https://github.com/sampctech/1 "$HOME"/github/1
-git clone https://github.com/sampctech/2  "$HOME"/github/2
-git clone https://github.com/sampctech/game "$HOME"/github/game
-git clone https://github.com/sampctech/mlt "$HOME"/github/mlt
+#git clone https://github.com/sampctech/2  "$HOME"/github/2
+#git clone https://github.com/sampctech/game "$HOME"/github/game
+#git clone https://github.com/sampctech/mlt "$HOME"/github/mlt
 git clone https://github.com/sampctech/org "$HOME"/github/org
 
 git clone https://github.com/arcolinuxd/arco-cinnamon "$HOME"/
