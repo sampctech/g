e0f81a2781d3113ec9ddcb1937ccaa131f047874
Sam Barna <54453917+sampctech@users.noreply.github.com>
2020-08-07 10:36:56 -0400

    Update getgit.sh
    Updated getgit file to include all the repositories I've created & 2 that I work with to keep my repos updated as best I can.

--- getgit.sh
+++ getgit.sh
@@ -6,13 +6,37 @@
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #####################################################################
 
-mkdir 0
-cd 0
-git clone https://github.com/sampctech/g "$HOME"/a
-git clone https://github.com/sampctech/arccin "$HOME"/arccin
-git clone https://github.com/sampctech/org "$HOME"/org
+#cd g
 
-git clone https://github.com/arcolinuxd/arco-cinnamon "$HOME"/arco-cinnamon
+echo "##############################################################"
+echo "####                  sampctech Repos                     ####"
+echo "##############################################################"
 
-chmod -R 755 "$HOME"/github
+git clone https://github.com/sampctech/a
+git clone https://github.com/sampctech/ab_renamer
+git clone https://github.com/sampctech/arccin
+git clone https://github.com/sampctech/arcob "arcobsam"
+git clone https://github.com/sampctech/gimpcon
+git clone https://github.com/sampctech/git
+git clone https://github.com/sampctech/githu
+git clone https://github.com/sampctech/m20
+git clone https://github.com/sampctech/org
+git clone https://github.com/sampctech/sets
+git clone https://github.com/sampctech/w10
+
+echo "##############################################################"
+echo "####                  ArcoLinux Repos                     ####"
+echo "##############################################################"
+
+git clone https://github.com/arcolinuxd/arco-cinnamon "arcolinuxd"
+git clone https://github.com/arcolinuxb/arco-cinnamon "arcolinuxb"
+
+#chmod -R 755 "$HOME"/g
+#chmod -vR +777 "$HOME/g"
+chmod -cR +777 "$HOME/g"
+
+echo "##############################################################"
+echo "####        Completed Clone and permission updates        ####"
+echo "##############################################################"
+
 exit
